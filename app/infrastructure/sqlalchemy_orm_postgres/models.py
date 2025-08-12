0fd4aab7c98fbf3add7c63fb04b9b8d18a515f97
Ivan Svyatykh <Zlativansvyat@mail.ru>
2025-08-12 08:23:53 +0500

    прошелся форматировшиком

--- app/infrastructure/sqlalchemy_orm_postgres/models.py
+++ app/infrastructure/sqlalchemy_orm_postgres/models.py
@@ -1,23 +1,15 @@
 import uuid
 
-from sqlalchemy import (
-    Column,
-    String,
-    Integer,
-    Numeric
-
-)
+from sqlalchemy import Column, String, Integer, Numeric
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class UserTable(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(256), index=True, unique=True)
     # Для простоты примера де нормализуем таблицу
-    money = Column(Numeric(precision=10,scale= 2))
-
-
+    money = Column(Numeric(precision=10, scale=2))
