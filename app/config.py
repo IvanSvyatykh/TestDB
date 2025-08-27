54f214b9dc29e658d725a403e708799b087aa052
ISvyatyx <ISvyatyx@vostok-electra.ru>
2025-08-27 10:45:18 +0500

    перешел на uv

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql+asyncpg://admin:admin@localhost:5432/postgres")
